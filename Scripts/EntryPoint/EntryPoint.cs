73b8e7a147ffe0b1376a64ad137bfc7d93819f2d
FirstSymbol <firstsymbol.0@gmail.com>
2025-07-05 19:46:22 +0300

    EntryPoind and dependencies

---
+++ Scripts/EntryPoint/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using ExtInspectorTools;
+
+namespace InfrastructurePackage
+{
+  public class EntryPoint : MonoBehaviour
+  {
+    [SerializeField, RequireNotNull] public EntryPointBase _entryPoint;
+    
+    private void Awake()
+    {
+      _entryPoint.AwakeEnter();
+    }
+
+    private void Start()
+    {
+      _entryPoint.StartEnter();
+    }
+  }
+}
